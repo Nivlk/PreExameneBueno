be8de9a927b98459a11e32bccba0c7e7fd15b249
SrWilly007 <aldairreal012@gmail.com>
2021-10-26 09:36:43 -0400

    Ejercicio 1 resuelto

--- Mayor.cs
+++ Mayor.cs
@@ -8,13 +8,18 @@ namespace mayor
         {
             int a;
             int b;
-            int c;
-            Console.WriteLine("Dar valor A");
+
+            Console.WriteLine("Ingrese el primero valor");
             a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Dar valor B");
+            Console.WriteLine("Ingrese el segundo valor");
             b = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Dar valor C");
-            c = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine ("{0} es mayor",a);
+            }
+            else 
+                Console.WriteLine ("{0} es mayor", b);
         }
     }
 }
