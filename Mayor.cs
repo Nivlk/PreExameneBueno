1b8bf9f72261ef7a73a0a31099b6e49c6c04a90e
SrWilly007 <aldairreal012@gmail.com>
2021-10-26 09:25:26 -0400

    Ejercicio 1 resolviendo

---
+++ Mayor.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace mayor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+            Console.WriteLine("Dar valor A");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dar valor B");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dar valor C");
+            c = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
